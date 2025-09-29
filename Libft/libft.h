3b4c3eec4ffc4967bcd70440c62f6b667752cc97
Edugs94 <edugs94@gmail.com>
2025-09-29 18:44:12 +0200

    Update until ft_strrchr

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: egalindo <egalindo@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 10:20:13 by egalindo          #+#    #+#             */
-/*   Updated: 2025/09/28 17:54:50 by egalindo         ###   ########.fr       */
+/*   Updated: 2025/09/29 18:28:34 by egalindo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,9 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 
 #endif
