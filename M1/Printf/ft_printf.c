ee9ca5906ae9228a36de971eea2aee88b4439961
Edugs94 <edugs94@gmail.com>
2025-10-22 23:10:51 +0200

    2nd attempt

--- M1/Printf/ft_printf.c
+++ M1/Printf/ft_printf.c
@@ -5,97 +5,84 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: egalindo <egalindo@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/13 14:17:47 by egalindo          #+#    #+#             */
-/*   Updated: 2025/10/21 21:56:37 by egalindo         ###   ########.fr       */
+/*   Created: 2025/10/22 21:38:20 by egalindo          #+#    #+#             */
+/*   Updated: 2025/10/22 21:38:20 by egalindo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft/libft.h"
 
-static int	ft_print_specifier(char specifier, unsigned long arg)
+static int	ft_print_specifier(char specifier, va_list args)
 {
 	int	len;
 
 	len = 0;
 	if (specifier == 'c')
-		len = ft_print_char((int)arg);
+		len = ft_print_char(va_arg(args, int));
 	else if (specifier == 's')
-		len = ft_print_str((char *)arg);
+		len = ft_print_str(va_arg(args, char *));
 	else if (specifier == 'p')
-		len = ft_print_pointer((void *)arg);
+		len = ft_print_pointer(va_arg(args, void *));
 	else if (specifier == 'd' || specifier == 'i')
-		len = ft_print_integer((int)arg);
+		len = ft_print_integer(va_arg(args, int));
 	else if (specifier == 'u')
-		len = ft_print_unsigned((unsigned int)arg);
+		len = ft_print_unsigned(va_arg(args, unsigned int));
 	else if (specifier == 'x')
-		len = ft_print_hex((unsigned int)arg);
+		len = ft_print_hex(va_arg(args, unsigned int));
 	else if (specifier == 'X')
-		len = ft_print_upper_hex((unsigned int)arg);
+		len = ft_print_upper_hex(va_arg(args, unsigned int));
 	return (len);
 }
 
-static int	ft_isformat(char c)
+static int	ft_aux_specifier(char spec, va_list args)
 {
-	if (c == 'c' || c == 's' || c == 'p' || c == 'd')
-		return (1);
-	else if (c == 'i' || c == 'u' || c == 'x' || c == 'X')
-		return (1);
-	else if (c == '%')
-		return (2);
-	else
-		return (0);
+	int	check;
+
+	if (!spec || spec == ' ')
+		return (-1);
+	if (spec == '%')
+		return (write(1, "%", 1));
+	if (ft_strchr("cspdiuxX", spec))
+	{
+		check = ft_print_specifier(spec, args);
+		if (check < 0)
+			return (-1);
+		return (check);
+	}
+	return (write(1, "%", 1) + write(1, &spec, 1));
 }
 
-static int	ft_check(char const *format, unsigned int *i, unsigned long arg)
+static int	iterate_arg(const char *format, int len, va_list args)
 {
-	int	len;
+	int		check;
 
-	*i = *i + 1;
-	len = 0;
-	len = ft_print_specifier(format[*i], arg);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			check = ft_aux_specifier(*format, args);
+			if (check < 0)
+				return (-1);
+			len += check;
+		}
+		else
+			len += write(1, format, 1);
+		format++;
+	}
 	return (len);
 }
 
 int	ft_printf(const char *format, ...)
 {
 	va_list			args;
-	unsigned int	i;
 	int				len;
-	int				check;
-	unsigned long	ulong;
 
-	i = 0;
-	len = 0;
 	va_start (args, format);
 	if (!format)
 		return (-1);
-	while (format[i])
-	{
-		if (format[i] == '%' && !format[i + 1])
-			return (-1);
-		else if (format[i] == '%' && ft_isformat(format[i + 1]) == 1)
-		{
-			ulong = va_arg(args, unsigned long);
-			check = ft_check(format, &i, ulong);
-			if (check < 0)
-				return (-1);
-			len += check;
-		}
-		else if (format[i] == '%' && ft_isformat(format[i + 1]) == 2)
-		{
-			len += write(1, "%", 1);
-			i++;
-		}
-		else if (format[i] == '%' && ft_isformat(format[i + 1]) == 0)
-		{
-			len += write(1, &format[i++], 1);
-			len += write(1, &format[i], 1);
-		}
-		else
-			len += write(1, &format[i], 1);
-		i++;
-	}
+	len = iterate_arg(format, 0, args);
 	va_end(args);
 	return (len);
 }
