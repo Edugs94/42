dc55fd84aaea91520a46662482d5eca5bd281150
Edugs94 <edugs94@gmail.com>
2025-10-28 20:32:56 +0100

    redoing logic get_next_line

--- M1/Get_next_line/get_next_line.c
+++ M1/Get_next_line/get_next_line.c
@@ -11,18 +11,8 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-/*
-	1. Leer con buffer_size
-	2. Almacenar todo en nueva linea
-	2.1 Hacer join de la nueva linea con la vieja linea
-	3. Si encuentra salto de linea hacer substr 
-		3.1 Lo que hay antes del salto de linea almacenarlo en second_half
-		3.2 el resto apuntarlo con puntero estático
-	4. Si no encuentra salto de linea ni null almacenar en second_half
-	5. Si no ha encontrado salto de linea volver a hacer el proceso de arriba
-*/
 
-static char	*read_and_build(char *second_half, char *buffer, int fd)
+static char	*read_and_build(char *first_half, char *second_half, char *buffer, int fd)
 {
 	ssize_t		bytes;
 	char		*tmp_cpy;
@@ -31,38 +21,42 @@ static char	*read_and_build(char *second_half, char *buffer, int fd)
 	if (bytes <= 0)
 		return (NULL);
 	else
-	{
-		tmp_cpy = ft_calloc( 1, ft_strlen(second_half) + 1);
+	{//replantear todo esto, 
+		tmp_cpy = ft_calloc( 1, BUFFER_SIZE + 1);
+		if (!first_half && (ft_strchr(second_half,'\n') != NULL) )
+			first_half = ft_strjoin(second_half, buffer);
 		strcpy (tmp_cpy, second_half);
-		free(second_half); // esto hace que pete
-		second_half = ft_strjoin(tmp_cpy, buffer);
+		first_half = ft_strjoin(tmp_cpy, buffer);
 		free(tmp_cpy);
-		if (!second_half)
+		if (!first_half)
 			return (NULL);
-		if (ft_strchr(second_half,'\n') != NULL)
-			return (second_half);
+		if (ft_strchr(first_half,'\n') != NULL)
+			return (first_half);
 		else
-			second_half = read_and_build(second_half, buffer, fd);
+			first_half = read_and_build(first_half, second_half, buffer, fd);
+			//revisar esto, tengo que construir la linea completa si el buffer no abarca sufficiente
 	}
-	return (second_half);
+	return (first_half);
 }
 
-char	*ft_trim(char *str, char *second_half)
+char	*ft_trim(char *first_half, char *second_half)
 {
-	char	*result;
+	size_t	i;
+	size_t	j;
 
-	result = ft_calloc( 1, ft_strlen(str) + 1);
-	if (!result)
-		return (NULL);
-	while (*str != '\n')
+	i = 0;
+	j = 0;
+	while (first_half[i] != '\n')
+			i++;
+	first_half[i] = '\0';
+	i++;
+	while (first_half[i] == '\n')
+		i++;
+	while (first_half[i + j] != '\0')
 	{
-			*result = *str;
-			result++;
-			str++;
+		second_half[j] = first_half[i + j];
+		j++;
 	}
-	while (*str == '\n')
-		str++;
-	second_half = str;
 	return (second_half);
 }
 
@@ -70,11 +64,11 @@ char	*get_next_line(int fd)
 {
 	char		*first_half;
 	static char	*second_half;
-	char		buffer[BUFFER_SIZE];
+	char		buffer[BUFFER_SIZE + 1];
 
 	if (!second_half)
 	{
-		second_half = ft_calloc(1, BUFFER_SIZE * 2 * sizeof(char) + 1);
+		second_half = ft_calloc(1, BUFFER_SIZE * sizeof(char)+ 1);
 		if (!second_half)
 			return (NULL);
 	}
@@ -82,16 +76,13 @@ char	*get_next_line(int fd)
 	first_half = ft_calloc(1, BUFFER_SIZE * sizeof(char) + 1);
 	if (!first_half)
 		return (NULL);
-	first_half[BUFFER_SIZE] = '\0';
-	first_half = read_and_build(second_half, buffer, fd);
+	first_half = read_and_build(first_half, second_half, buffer, fd);
 	if (ft_strchr(first_half,'\n') != NULL)
 	{
 		second_half = ft_calloc(1, BUFFER_SIZE + 1);
 		if(second_half == NULL)
 			return (NULL);
 		second_half = ft_trim(first_half, second_half);
-		if(second_half == NULL)
-			return (NULL);
 	}
 	return (first_half);
 }
