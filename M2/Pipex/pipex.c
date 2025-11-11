c0b4a4f723cbfdcbf35a02620c1c1ff9227c911e
Eduardo Galindo Salazar <egalindo@car8s6.42barcelona.com>
2025-11-11 17:05:11 +0100

    changes on readme born2beroot

--- M2/Pipex/pipex.c
+++ M2/Pipex/pipex.c
@@ -6,13 +6,82 @@
 /*   By: egalindo <egalindo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 08:05:40 by egalindo          #+#    #+#             */
-/*   Updated: 2025/11/10 09:28:37 by egalindo         ###   ########.fr       */
+/*   Updated: 2025/11/11 17:04:10 by egalindo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+void	exit_handler(int n)
 {
+	if (n == 1)
+	{
+		//mejor usar ft_putstr_fd
+		ft_printf("Incorrect quantity of arguments, please make sure");
+		ft_printf("to introduce:\n");
+		ft_printf("./pipex infile cmd1 cmd2 outfile\n");
+	}
+	exit(0);
+}
+
+char	*get_path(char **env)
+{
+}
+
+int	execute (char *cmd, char **env)
+{
+	char	**cmd_splitted;
+	char	*path;
 	
+	path = get_path(env);// asi??
+	cmd_splitted = ft_split(cmd);
+	execve(path, cmd_splitted, env);
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	int		fd[2];
+	pid_t	pid1;
+	pid_t	pid2;
+	int		infile;
+	int		outfile;
+	char	*cmd1 = argv[2];
+	char	*cmd2 = argv[3];
+
+	//arg_checker(argc, argv);
+	if (argc != 5)
+		exit_handler(1);
+	if (pipe(fd) == -1)
+		return (-1);
+	pid1 =fork();
+	if (pid1 < 0) // falla creacion de fork
+		exit(-1);
+	if (pid1 == 0) //child
+	{
+		infile = open(argv[1], O_RDONLY);
+		if (infile == -1)
+			exit(1);
+		dup2(infile, 0);
+		dup2(fd[1], 1);
+		close(fd[0]);
+		execute(cmd1, env);
+	}
+	pid2 = fork();
+	if (pid2 < 0)
+		exit(-1);
+	if (pid2 == 0)
+	{
+		outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+		if (outfile == -1)
+			exit(1);
+		dup2(outfile, 1);
+		dup2(fd[0], 0);
+		close(fd[1]);
+		execute(cmd2, env);
+	}
+	close(fd[0]);
+	close(fd[1]);
+	wait(pid1, NULL, 0);
+	wait(pid2, NULL, 0);
+	return (0);
 }
